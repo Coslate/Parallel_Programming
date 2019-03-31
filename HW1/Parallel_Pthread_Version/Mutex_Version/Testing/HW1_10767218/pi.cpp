17cb3f80eaae7fd932d9d51f8c23633dd7f36c4a
Coslate <coslate@media.ee.ntu.edu.tw>
2019-03-31 19:51:21 +0800

    Revise

--- HW1/Parallel_Pthread_Version/Mutex_Version/Testing/HW1_10767218/pi.cpp
+++ HW1/Parallel_Pthread_Version/Mutex_Version/Testing/HW1_10767218/pi.cpp
@@ -9,19 +9,23 @@
 #include <pthread.h>
 #include <stdint.h>
 
+#define RAND_MAX_HALF_PRIV (RAND_MAX/2)
+
 //Shared Variables
 long long int number_in_circles     = 0;
 long long int number_of_tosses      = 0;
-unsigned  int thread_num            = 0;
+long  int     thread_num            = 0;
 pthread_mutex_t mutex;
 long long int each_thread_work_load = 0;
 
+/*
 inline void PrintHelp(){
     std::cerr<<"Error: This pi.o takes two input arguments."<<std::endl;
     std::cerr<<"       Please use as the format: ./pi.o [arg1] [arg1]"<<std::endl;
     std::cerr<<"       [arg1]: The number of cores that you want to run this program on."<<std::endl;
     std::cerr<<"       [arg2]: The number of tosses that you want to make for the estimation."<<std::endl;
 }
+*/
 
 void *ThreadFunction(void *rank){
     long int thread_id          = (long int) rank;
@@ -29,11 +33,11 @@ void *ThreadFunction(void *rank){
     unsigned int thread_seed    = (unsigned int) thread_id;
 
     for(long long int i=0;i<each_thread_work_load;++i){
-        float x = -1 + (float)(rand_r(&thread_seed))/(float)(RAND_MAX)*2.f;
-        float y = -1 + (float)(rand_r(&thread_seed))/(float)(RAND_MAX)*2.f;
+        float x = -1.f + (float)(rand_r(&thread_seed))/(float)(RAND_MAX_HALF_PRIV);
+        float y = -1.f + (float)(rand_r(&thread_seed))/(float)(RAND_MAX_HALF_PRIV);
         float distance = x*x+y*y;
 
-        if(distance <= 1){
+        if(distance <= 1.f){
             ++my_local_sum;
         }
     }
@@ -45,13 +49,13 @@ void *ThreadFunction(void *rank){
 }
 
 int main(int argc, char *argv[]){
-    if(argc != 3){
+/*    if(argc != 3){
         PrintHelp();
         return EXIT_FAILURE;
-    }
+    }*/
 
     float pi_esimate      = 0;
-    long int seed_t       = time(NULL);
+    //long int seed_t       = time(NULL);
 
     thread_num                = std::stoi(argv[1]);
     number_of_tosses          = std::stoll(argv[2]);
@@ -59,19 +63,19 @@ int main(int argc, char *argv[]){
     pthread_t *thread_handles = new pthread_t [thread_num-1];
     pthread_mutex_init(&mutex, NULL);
 
-    for(unsigned int thread_id=0;thread_id<thread_num-1;++thread_id){
-        pthread_create(&thread_handles[thread_id], NULL, ThreadFunction, (void *)(seed_t+thread_id));
+    for(long int thread_id=0;thread_id<thread_num-1;++thread_id){
+        pthread_create(&thread_handles[thread_id], NULL, ThreadFunction, (void *)(thread_id));
     }
 
-    ThreadFunction((void *)(seed_t+thread_num));
+    ThreadFunction((void *)(thread_num));
 
-    for(unsigned int thread_id=0;thread_id<thread_num-1;++thread_id){
+    for(long int thread_id=0;thread_id<thread_num-1;++thread_id){
         pthread_join(thread_handles[thread_id], NULL);
     }
 
-    pi_esimate = 4*((float)number_in_circles)/(float)(number_of_tosses);
+    pi_esimate = 4.f*((float)number_in_circles)/(float)(number_of_tosses);
 
-    std::cout<<std::fixed<<pi_esimate<<std::endl;
+    printf("%.6f\n", pi_esimate);
     delete thread_handles;
     pthread_mutex_destroy(&mutex);
     return EXIT_SUCCESS;
