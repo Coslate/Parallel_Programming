7ee10d44b4fdac551a1e9966776673bf5d27aedf
Coslate <coslate@media.ee.ntu.edu.tw>
2019-04-28 22:03:34 +0800

    Update initialization part

--- HW3/Parallel_MPI_Version/conduction.init_bcast.cpp
+++ HW3/Parallel_MPI_Version/conduction.init_bcast.cpp
@@ -12,46 +12,55 @@ int main(int argc, char **argv) {
     int my_rank;  //the id of the current process
     int nprocess; //total number of processes
     double time0, time1;
+    srand(seed);
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocess);
 
-    //time0 = MPI_Wtime();//start timing
+    time0 = MPI_Wtime();//start timing
     //-----------Argument Parser----------//
     N = atoi(argv[1]);
     seed = atoi(argv[2]);
-    srand(seed);
     //printf("N in %d of %d is %d, seed = %d\n", my_rank, nprocess, N, seed);
-    //time1 = MPI_Wtime();
+    time1 = MPI_Wtime();
     //printf("Argument Parser using time: %lf\n", (time1-time0));
 
     //-----------Initialization----------//
-    //time0 = MPI_Wtime();
+    time0 = MPI_Wtime();
     int temp[N][N];
-
-    srand(seed);
     if(my_rank == 0){
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 temp[i][j] = random() >> 3; // avoid overflow
             }
         }
-        /*Print for debug
+        printf("Before Broadcast...\n");
+        printf("rank %d, N = \n", my_rank);
         for(int i=0;i<N;++i){
             for(int j=0;j<N;++j){
-                printf("%d ", temp[i][j]);
-                if(j==N-1){
+                printf("%2d ", temp[i][j]);
+                if(j==(N-1)){
                     printf("\n");
                 }
             }
         }
-        */
     }
 
     MPI_Bcast(temp, N*N, MPI_INT, 0, MPI_COMM_WORLD);
-    //time1 = MPI_Wtime();
-    //printf("Initialization using time: %lf\n", (time1-time0));
+    time1 = MPI_Wtime();
+
+    printf("After Broadcast...\n");
+    printf("rank %d, N = \n", my_rank);
+    for(int i=0;i<N;++i){
+        for(int j=0;j<N;++j){
+            printf("%2d ", temp[i][j]);
+            if(j==(N-1)){
+                printf("\n");
+            }
+        }
+    }
+    printf("Initialization using time: %lf\n", (time1-time0));
     //-----------Main Calculation----------//
     time0 = MPI_Wtime();
     int count = 0, balance = 0;
@@ -66,7 +75,7 @@ int main(int argc, char **argv) {
                 int left = j - 1 < 0 ? 0 : j - 1;
                 int right = j + 1 >= N ? j : j + 1;
                 next[i][j] = (temp[i][j] + temp[up][j] + temp[down][j] +
-                            temp[i][left] + temp[i][right]) / 5;
+                    temp[i][left] + temp[i][right]) / 5;
                 if (next[i][j] != temp[i][j]) {
                     balance = 0;
                 }
@@ -75,8 +84,9 @@ int main(int argc, char **argv) {
         memcpy(temp, next, N * N * sizeof(int));
     }
     time1 = MPI_Wtime();
-    printf("Size: %d*%d, Seed: %d, ", N, N, seed);
-    printf("Iteration: %d, Temp: %d\n", count, temp[0][0]);
+
+    //printf("Size: %d*%d, Seed: %d, ", N, N, seed);
+    //printf("Iteration: %d, Temp: %d\n", count, temp[0][0]);
     //printf("Main Calculation using time in rank %d: %lf\n", my_rank, (time1-time0));
 
     MPI_Finalize();
