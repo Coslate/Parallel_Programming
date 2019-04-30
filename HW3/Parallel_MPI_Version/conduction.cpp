b2c55c0e43a7292e5297c369663957b3088fdf73
Coslate <coslate@media.ee.ntu.edu.tw>
2019-05-01 01:57:04 +0800

    Need to solve problem that one cluster finish first.

--- HW3/Parallel_MPI_Version/conduction.cpp
+++ HW3/Parallel_MPI_Version/conduction.cpp
@@ -24,7 +24,7 @@ int main(int argc, char **argv) {
     N = atoi(argv[1]);
     seed = atoi(argv[2]);
     srand(seed);
-    //printf("N in %d of %d is %d, seed = %d\n", my_rank, nprocess, N, seed);
+    printf("N in %d of %d processes is %d, seed = %d\n", my_rank, nprocess, N, seed);
     //time1 = MPI_Wtime();
     //printf("Argument Parser using time: %lf\n", (time1-time0));
 
@@ -57,40 +57,55 @@ int main(int argc, char **argv) {
     
     //-----------Main Calculation----------//
     time0 = MPI_Wtime();
-    int count = 0, balance = 0;
-    int nworkers   = nprocess-1;
-    int avg_rows   = N/nworkers;
-    int extra_rows = N%nworkers;
-    int offset     = 0;
-    int rows;
-    int next[N][N];
+    MPI_Status status;
+    int count          = 0;
+    int balance        = 0;
+    int avg_rows       = N/nprocess;
+    int extra_rows     = N%nprocess;
+    int *my_rank_start = new int [nprocess];
+    int *my_rank_end   = new int [nprocess];
+    int *my_rank_rows  = new int [nprocess];
+    int next_tmp[(avg_rows+1)][N];
+    int next[N][N]     = { {0} };
+    int start_accum    = 0;
 
-    while (!balance) {
-        count++;
-        balance = 1;
-
-        if(my_rank == MASTER){//Master
-            if(count > 0){
-                for(int dest=1;dest<=nworkers;++dest){
-                    rows = (dest<extra_rows)?(avg_rows+1):avg_rows;
-                    MPI_Send(&offset, 1, MPI_INT, dest, FROM_MASTER, MPI_COMM_WORLD);
-                    MPI_Send(&rows, 1, MPI_INT, dest, FROM_MASTER, MPI_COMM_WORLD);
-                    MPI_Send(&temp[offset][0], rows*N, MPI_INT, dest, FROM_MASTER, MPI_COMM_WORLD);
-        
-                }
+    printf("Original temp with rank %d is: \n", my_rank);
+    for(int i=0;i<N;++i){
+        for(int j=0;j<N;++j){
+            printf("%d ", temp[i][j]);
+            if(j==N-1){
+                printf("\n");
             }
-        }else{//Workers
-    
         }
     }
     
+    //work division
+    for(int i=0;i<nprocess;++i){
+        int i_rows = (i<(extra_rows))?(avg_rows+1):avg_rows;
+        my_rank_start[i] = (i==0)?0:my_rank_end[i-1]+1;
+        my_rank_end[i]   = my_rank_start[i]+(i_rows-1);
+        my_rank_rows[i]  = i_rows;
+    }
 
-
-    //Workers
     while (!balance) {
         count++;
         balance = 1;
-        for (int i = 0; i < N; i++) {
+
+        if(count > 1){
+            MPI_Bcast(temp, N*N, MPI_INT, 0, MPI_COMM_WORLD);
+        }
+        
+        printf("Iteration %d, rank = %d, temp = \n", count, my_rank);
+        for(int i=0;i<N;++i){
+            for(int j=0;j<N;++j){
+                printf("%d ", temp[i][j]);
+                if(j==N-1){
+                    printf("\n");
+                }
+            }
+        }
+
+        for (int i = my_rank_start[my_rank]; i <= my_rank_end[my_rank]; i++) {
             for (int j = 0; j < N; j++) {
                 int up = i - 1 < 0 ? 0 : i - 1;
                 int down = i + 1 >= N ? i : i + 1;
@@ -103,11 +118,42 @@ int main(int argc, char **argv) {
                 }
             }
         }
-        memcpy(temp, next, N * N * sizeof(int));
-    }
+
+        printf("Iteration %d, rank = %d, next = \n", count, my_rank);
+        for(int i=0;i<N;++i){
+            for(int j=0;j<N;++j){
+                printf("%d ", next[i][j]);
+                if(j==N-1){
+                    printf("\n");
+                }
+            }
+        }
+
+        if(my_rank == MASTER){//Master, combine all the next[][]
+            int balance_from_others = 1;
+
+            for(int i=1;i<nprocess;++i){
+                MPI_Recv(&next_tmp[0][0], (avg_rows+1)*N, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+                int sender_rank = status.MPI_SOURCE;
+                MPI_Recv(&balance_from_others, 1, MPI_INT, sender_rank, 0, MPI_COMM_WORLD, &status);
+                memcpy(&temp[my_rank_start[sender_rank]][0], next_tmp, my_rank_rows[sender_rank] * N * sizeof(int));
+                balance = balance*balance_from_others;
+            }
+            //copy the result of Master next[][]
+            memcpy(&temp[my_rank_start[MASTER]][0], next, my_rank_rows[MASTER] * N * sizeof(int));
+        }else{//Workers, send the result : next[][]
+            MPI_Send(&next[my_rank_start[my_rank]][0], my_rank_rows[my_rank]*N, MPI_INT, MASTER, 0, MPI_COMM_WORLD);
+            MPI_Send(&balance, 1, MPI_INT, MASTER, 0, MPI_COMM_WORLD);
+        }
+        printf("rank = %d, count = %d, balance = %d\n", my_rank, count, balance);
+        //char ch = getchar();
+    }//end while
+    
     time1 = MPI_Wtime();
-    printf("Size: %d*%d, Seed: %d, ", N, N, seed);
-    printf("Iteration: %d, Temp: %d\n", count, temp[0][0]);
+    if(my_rank == MASTER){
+        printf("Size: %d*%d, Seed: %d, ", N, N, seed);
+        printf("Iteration: %d, Temp: %d\n", count, temp[0][0]);
+    }
     //printf("Main Calculation using time in rank %d: %lf\n", my_rank, (time1-time0));
 
     MPI_Finalize();
