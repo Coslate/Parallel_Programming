b04d1313f426666ae984ef08fdffe6b7fb9539a8
Coslate <coslate@media.ee.ntu.edu.tw>
2019-05-26 13:07:28 +0800

    Update serial ver.

--- HW5/Serial/histogram.cpp
+++ HW5/Serial/histogram.cpp
@@ -1,68 +1,168 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <ios>
 
-unsigned int * histogram(unsigned int *image_data, unsigned int _size) {
+typedef struct
+{
+    uint8_t R;
+    uint8_t G;
+    uint8_t B;
+    uint8_t align;
+} RGB;
 
-	unsigned int *img = image_data;
-	unsigned int *ref_histogram_results;
-	unsigned int *ptr;
+typedef struct
+{
+    bool type;
+    uint32_t size;
+    uint32_t height;
+    uint32_t weight;
+    RGB *data;
+} Image;
 
-	ref_histogram_results = (unsigned int *)malloc(256 * 3 * sizeof(unsigned int));
-	ptr = ref_histogram_results;
-	memset (ref_histogram_results, 0x0, 256 * 3 * sizeof(unsigned int));
+Image *readbmp(const char *filename)
+{
+    std::ifstream bmp(filename, std::ios::binary);
+    char header[54];
+    bmp.read(header, 54);
+    uint32_t size = *(int *)&header[2];
+    uint32_t offset = *(int *)&header[10];
+    uint32_t w = *(int *)&header[18];
+    uint32_t h = *(int *)&header[22];
+    uint16_t depth = *(uint16_t *)&header[28];
+    if (depth != 24 && depth != 32)
+    {
+        printf("we don't suppot depth with %d\n", depth);
+        exit(0);
+    }
+    bmp.seekg(offset, bmp.beg);
 
-	// histogram of R
-	for (unsigned int i = 0; i < _size; i += 3)
-	{
-		unsigned int index = img[i];
-		ptr[index]++;
-	}
-
-	// histogram of G
-	ptr += 256;
-	for (unsigned int i = 1; i < _size; i += 3)
-	{
-		unsigned int index = img[i];
-		ptr[index]++;
-	}
-
-	// histogram of B
-	ptr += 256;
-	for (unsigned int i = 2; i < _size; i += 3)
-	{
-		unsigned int index = img[i];
-		ptr[index]++;
-	}
-
-	return ref_histogram_results;
+    Image *ret = new Image();
+    ret->type = 1;
+    ret->height = h;
+    ret->weight = w;
+    ret->size = w * h;
+    ret->data = new RGB[w * h];
+    for (int i = 0; i < ret->size; i++)
+    {
+        bmp.read((char *)&ret->data[i], depth / 8);
+    }
+    return ret;
 }
 
-int main(int argc, char const *argv[])
+int writebmp(const char *filename, Image *img)
 {
 
-	unsigned int * histogram_results;
-	unsigned int i=0, a, input_size;
-	std::fstream inFile("input", std::ios_base::in);
-	std::ofstream outFile("10767218.out", std::ios_base::out);
+    uint8_t header[54] = {
+        0x42,        // identity : B
+        0x4d,        // identity : M
+        0, 0, 0, 0,  // file size
+        0, 0,        // reserved1
+        0, 0,        // reserved2
+        54, 0, 0, 0, // RGB data offset
+        40, 0, 0, 0, // struct BITMAPINFOHEADER size
+        0, 0, 0, 0,  // bmp width
+        0, 0, 0, 0,  // bmp height
+        1, 0,        // planes
+        32, 0,       // bit per pixel
+        0, 0, 0, 0,  // compression
+        0, 0, 0, 0,  // data size
+        0, 0, 0, 0,  // h resolution
+        0, 0, 0, 0,  // v resolution
+        0, 0, 0, 0,  // used colors
+        0, 0, 0, 0   // important colors
+    };
 
-	inFile >> input_size;
-	unsigned int *image = new unsigned int[input_size];
-	while( inFile >> a ) {
-		image[i++] = a;
-	}
+    // file size
+    uint32_t file_size = img->size * 4 + 54;
+    header[2] = (unsigned char)(file_size & 0x000000ff);
+    header[3] = (file_size >> 8) & 0x000000ff;
+    header[4] = (file_size >> 16) & 0x000000ff;
+    header[5] = (file_size >> 24) & 0x000000ff;
 
-	histogram_results = histogram(image, input_size);
-	for(unsigned int i = 0; i < 256 * 3; ++i) {
-		if (i % 256 == 0 && i != 0)
-			outFile << std::endl;
-		outFile << histogram_results[i]<< ' ';
-	}
+    // width
+    uint32_t width = img->weight;
+    header[18] = width & 0x000000ff;
+    header[19] = (width >> 8) & 0x000000ff;
+    header[20] = (width >> 16) & 0x000000ff;
+    header[21] = (width >> 24) & 0x000000ff;
 
-	inFile.close();
-	outFile.close();
+    // height
+    uint32_t height = img->height;
+    header[22] = height & 0x000000ff;
+    header[23] = (height >> 8) & 0x000000ff;
+    header[24] = (height >> 16) & 0x000000ff;
+    header[25] = (height >> 24) & 0x000000ff;
 
-	return 0;
+    std::ofstream fout;
+    fout.open(filename, std::ios::binary);
+    fout.write((char *)header, 54);
+    fout.write((char *)img->data, img->size * 4);
+    fout.close();
+}
+
+void histogram(Image *img,uint32_t R[256],uint32_t G[256],uint32_t B[256]){
+    std::fill(R, R+256, 0);
+    std::fill(G, G+256, 0);
+    std::fill(B, B+256, 0);
+
+    for (int i = 0; i < img->size; i++){
+        RGB &pixel = img->data[i];
+        R[pixel.R]++;
+        G[pixel.G]++;
+        B[pixel.B]++;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    char *filename;
+    if (argc >= 2)
+    {
+        int many_img = argc - 1;
+        for (int i = 0; i < many_img; i++)
+        {
+            filename = argv[i + 1];
+            Image *img = readbmp(filename);
+
+            std::cout << img->weight << ":" << img->height << "\n";
+
+            uint32_t R[256];
+            uint32_t G[256];
+            uint32_t B[256];
+
+            histogram(img,R,G,B);
+
+            int max = 0;
+            for(int i=0;i<256;i++){
+                max = R[i] > max ? R[i] : max;
+                max = G[i] > max ? G[i] : max;
+                max = B[i] > max ? B[i] : max;
+            }
+
+            Image *ret = new Image();
+            ret->type = 1;
+            ret->height = 256;
+            ret->weight = 256;
+            ret->size = 256 * 256;
+            ret->data = new RGB[256 * 256];
+
+            for(int i=0;i<ret->height;i++){
+                for(int j=0;j<256;j++){
+                    if(R[j]*256/max > i)
+                        ret->data[256*i+j].R = 255;
+                    if(G[j]*256/max > i)
+                        ret->data[256*i+j].G = 255;
+                    if(B[j]*256/max > i)
+                        ret->data[256*i+j].B = 255;
+                }
+            }
+
+            std::string newfile = "hist_" + std::string(filename); 
+            writebmp(newfile.c_str(), ret);
+        }
+    }else{
+        printf("Usage: ./hist <img.bmp> [img2.bmp ...]\n");
+    }
+    return 0;
 }
